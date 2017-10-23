b0320886fe8c8be69014082b2668230635ebaf52
sharonrussell <sharon.russell@comparethemarket.com>
2017-10-23 21:11:35 +0100

    Added capability to get notes from mongodb

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express')
+  , app = express()
+  MongoClient = require('mongodb').MongoClient,
+  assert = require('assert'),
+  ObjectId = require('mongodb').ObjectId;
+
+var port = 3000;
+var mongouri = 'mongodb://localhost:27017/notes';
+  
+MongoClient.connect(mongouri, function(err, db){
+  app.get('/notes', function (req, res){
+    db.collection('notes').find({}).sort({'date': -1}).toArray(function(err, docs){
+      res.send(docs);
+    });
+	});
+});
+
+app.listen(port, function() {
+  console.log('Listening on port ' + port)
+});
