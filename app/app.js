b4b67efed3fc4a824759a8470d200233c37d30eb
sharonrussell <sharon.russell@comparethemarket.com>
2017-11-03 10:12:25 +0000

    Moved things around and added docker

--- app/app.js
+++ app/app.js
@@ -5,15 +5,23 @@ var express = require('express')
   ObjectId = require('mongodb').ObjectId;
 
 var port = 3000;
-var mongouri = 'mongodb://localhost:27017/notes';
+var mongouri = 'mongodb://mongo:27017/notes';
   
 MongoClient.connect(mongouri, function(err, db){
   app.get('/notes', function (req, res){
     db.collection('notes').find({}).toArray(function(err, docs){
+      
+      if (err) return next(err); 
+
       res.header("Access-Control-Allow-Origin", "*");
       res.send(docs);
     });
-	});
+  });
+
+  app.post('/delete/:id', function (req, res) {
+    var id = req.params.id;
+    db.collection('entries').remove({"_id": ObjectId(id)});
+  });
 });
 
 app.listen(port, function() {
