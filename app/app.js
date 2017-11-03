546c8fd9a86bc84e2f0062bf5933018239463dfb
sharonrussell <sharon.russell@comparethemarket.com>
2017-11-03 14:27:02 +0000

    Added headers to node app to allow origin I'm working on for other app

--- app/app.js
+++ app/app.js
@@ -8,12 +8,31 @@ var port = 3000;
 var mongouri = 'mongodb://mongo:27017/notes';
   
 MongoClient.connect(mongouri, function(err, db){
+  // Add headers
+  app.use(function (req, res, next) {
+  
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+  
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+      // Set to true if you need the website to include cookies in the requests sent
+      // to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+  
+      // Pass to next layer of middleware
+      next();
+  });
+
   app.get('/notes', function (req, res){
     db.collection('notes').find({}).toArray(function(err, docs){
       
       if (err) return next(err); 
 
-      res.header("Access-Control-Allow-Origin", "*");
       res.send(docs);
     });
   });
